39d8f2c0706cef9e1b7585d49855447a51181355
71446397 <baresipietro42@gmail.com>
2026-09-19 09:40:25 +0200

    Utilizzo classe Studente

---
+++ Prova_Classe_Studente/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Studente a, c;
+        a = new Studente();
+        c = new Studente();
+        a.nome = "Marco";
+        a.cognome = "Rossi";
+        a.eta = 21;
+        a.peso = 68.6;
+        a.altezza = 1.70;
+        a.classe = "1FI";
+        c.nome = "Fabio";
+        c.cognome = "Bianchi";
+        c.eta = 14;
+        c.peso = 60;
+        c.altezza = 1.90;
+        c.classe = "3AI";
+    }
+}
---
+++ Prova_Classe_Studente/src/Studente.java
@@ -0,0 +1,8 @@
+public class Studente {
+    String nome;
+    String cognome;
+    double altezza;
+    double peso;
+    int eta;
+    String classe;
+}
