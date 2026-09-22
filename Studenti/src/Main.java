c0d343ae522c131d0933cfc7d286170a4be63a86
71446397 <baresipietro42@gmail.com>
2026-09-22 12:50:42 +0200

    nuovo esercizio

---
+++ Studenti/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Studente s;
+        s = new Studente("Pietro", "Baresi", 16, 1.78);
+        System.out.println(s.nome);
+    }
+}
